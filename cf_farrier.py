e51adfb5ea5714559b55ddc0557ff4dc85efb8cd
Peter M. Landwehr <plandweh@cs.cmu.edu>
2017-05-03 00:25:29 -0700

    Create cf_farrier.py

---
+++ cf_farrier.py
@@ -0,0 +1,14 @@
+# TODO take package or list of packages
+# TODO take github url or github urls
+# TODO If user doesn't have a fork of conda-forge create one
+# TODO if user doesn't have a branch for this package, create one
+# TODO run conda smithy pypi package
+# TODO If successful, parse generated meta.yaml
+# TODO Drop any meta.yaml build reqs
+# TODO Drop any test reqs
+# TODO Add sections to bring in line with the standard template
+# TODO Add variable to bring in line with the standard template
+# TODO change the setup script based on whether or not setuptools is required
+# TODO deal with license file
+# TODO commit the modified meta.yaml to user/staged-recipes:branch
+# TODO submit pull request to conda-forge/staged-recipes:master as a new recipe.
